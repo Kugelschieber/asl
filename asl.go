d2c32b3b85ea825dfd3ac9f829c869a69e2992c6
nikolauska <nikolauska1@gmail.com>
2015-11-02 12:00:18 +0300

    added new repository layout and unified tests

--- asl.go
+++ asl.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"parser"
-	"tokenizer"
 	"fmt"
 	"io/ioutil"
 	"path/filepath"
@@ -15,6 +13,7 @@ const (
     extension = ".asl"
     sqfextension = ".sqf"
     PathSeparator = string(os.PathSeparator)
+	new_line = "\r\n"
 )
 
 type ASLFile struct {
@@ -43,7 +42,7 @@ func usage() {
 
 func flags(flag string) bool {
     flag = strings.ToLower(flag)
-    
+
     if flag[0] == '-' {
         if flag == "-v" {
             fmt.Println("asl version "+version)
@@ -56,24 +55,24 @@ func flags(flag string) bool {
             usage()
             exit = true
         }
-        
+
         return true
     }
-    
+
     return false
 }
 
 func readAslFiles(path string) {
     dir, err := ioutil.ReadDir(path)
-    
+
     if err != nil {
         fmt.Println("Error reading in directory!")
         return
     }
-    
+
     for i := 0; i < len(dir); i++ {
         name := dir[i].Name()
-        
+
         if dir[i].IsDir() && recursive {
             readAslFiles(filepath.FromSlash(path+PathSeparator+name))
             continue
@@ -82,8 +81,8 @@ func readAslFiles(path string) {
         if !dir[i].IsDir() && strings.ToLower(filepath.Ext(name)) == extension {
             in := filepath.FromSlash(path+PathSeparator+dir[i].Name())
             out := filepath.FromSlash("./"+path[len(inDir):len(path)])
-            newname := name[:len(name)-len(filepath.Ext(name))] 
-            
+            newname := name[:len(name)-len(filepath.Ext(name))]
+
             file := ASLFile{in, out, newname}
             aslFiles = append(aslFiles, file)
         }
@@ -95,19 +94,19 @@ func compile(path string) {
         out := filepath.FromSlash(path+PathSeparator+aslFiles[i].out+PathSeparator+aslFiles[i].newname+sqfextension)
         fmt.Println(aslFiles[i].in+" -> "+out)
         code, err := ioutil.ReadFile(aslFiles[i].in)
-        
+
         if err != nil {
             fmt.Println("Error reading file: "+aslFiles[i].in)
             continue
         }
-        
-    	token := tokenizer.Tokenize(code)
-    	compiler := parser.Compiler{}
+
+    	token := Tokenize(code)
+    	compiler := Compiler{}
     	sqf := compiler.Parse(token, pretty)
-    	
+
     	os.MkdirAll(filepath.FromSlash(path+PathSeparator+aslFiles[i].out), 0777)
     	err = ioutil.WriteFile(out, []byte(sqf), 0666)
-    	
+
     	if err != nil {
     	    fmt.Println("Error writing file: "+aslFiles[i].out)
     	    fmt.Println(err)
@@ -117,34 +116,34 @@ func compile(path string) {
 
 func main() {
 	args := os.Args
-	
+
 	// flags
 	if len(args) < 2 {
 	    usage()
 	    return
 	}
-	
+
 	var i int
 	for i = 1; i < len(args) && flags(args[i]); i++ {}
-	
+
 	if exit {
 	    return
 	}
-	
+
 	// in/out parameter
 	out := ""
-	
+
 	if i < len(args) {
 	    inDir = args[i]
 	    i++
 	} else {
 	    return
 	}
-	
+
 	if i < len(args) {
 	    out = args[i]
 	}
-	
+
 	readAslFiles(inDir)
 	compile(out)
 }
